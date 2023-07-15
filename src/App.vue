635abb20192869e5b07e960768de52e71dc8fa73
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-07-16 01:17:45 +0200

    Fix invalid keys

--- src/App.vue
+++ src/App.vue
@@ -261,7 +261,7 @@ onMounted(async () => {
     </div>
 
     <div id="animes">
-      <Anime v-for="anime in animes" :key="anime.lien" :anime="anime" :metadata="data.secondary[anime.mal_id]"/>
+      <Anime v-for="anime in animes" :key="anime.mal_id" :anime="anime" :metadata="data.secondary[anime.mal_id]"/>
     </div>
 
     <div v-if="animes.length === 0" id="noResults">
