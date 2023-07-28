6002d4b6b20cd88e6ae0c3e020d8d46be49df45c
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2023-07-28 16:56:23 +0200

    Implement a v2 of the API In addition to the data from v1, the following is included : - Synopsis - Start date - End date - Number of episodes - Studios - Release season

--- metadata-updater/updater.mjs
+++ metadata-updater/updater.mjs
@@ -1,4 +1,4 @@
-import { writeFileSync, createReadStream, createWriteStream } from 'fs';
+import { promises as fsPromise, createReadStream, createWriteStream, mkdirSync } from 'fs';
 import { createGzip, createBrotliCompress, constants as zlibConstants } from 'zlib';
 import { pipeline } from 'stream';
 import { promisify } from 'util';
@@ -19,7 +19,7 @@ async function fetchUserList(username) {
     while (hasMore) {
         console.log(`Fetching ${username} offset ${offset}`);
 
-        const response = await fetch(`https://api.myanimelist.net/v2/users/${username}/animelist?limit=100&fields=list_status,alternative_titles,media_type,status&offset=${offset}&nsfw=true`, {
+        const response = await fetch(`https://api.myanimelist.net/v2/users/${username}/animelist?limit=100&fields=list_status,alternative_titles,media_type,status,synopsis,start_date,end_date,num_episodes,start_season,studios&offset=${offset}&nsfw=true`, {
             headers: {
                 'X-MAL-CLIENT-ID': process.env.MAL_CLIENT_ID, // Authenticate with the MAL API
             }
@@ -84,61 +84,184 @@ const [tiralex, cycy, leo, gyrehio, tchm] = await Promise.all([
     fetchUserList('tchm'),
 ]);
 
-// Create a map of anime id to additional data
-const additionalData = {}
+/**
+ * Generates the API v1 data, also known as additional-data.json
+ */
+async function generateApiV1() {
+    // Create a map of anime id to additional data
+    const additionalData = {}
 
-// Add the data for each user
-for(const user of [tiralex, cycy, leo, gyrehio, tchm]) {
-    // Add the data for each anime
-    for(const anime of user) {
-        // Check if the anime is already in the map
-        if(additionalData[anime.node.id] === undefined) {
-            additionalData[anime.node.id] = {
-                scores: {},
-                cover: anime.node.main_picture.large !== undefined ? anime.node.main_picture.large : anime.node.main_picture.medium,
-                titles: { original: anime.node.title , ...anime.node.alternative_titles },
-                status: anime.node.status,
-                type: anime.node.media_type,
-                wasWatched: false,
-            };
-        }
+    // Add the data for each user
+    for(const user of [tiralex, cycy, leo, gyrehio, tchm]) {
+        // Add the data for each anime
+        for(const anime of user) {
+            // Check if the anime is already in the map
+            if(additionalData[anime.node.id] === undefined) {
+                additionalData[anime.node.id] = {
+                    scores: {},
+                    cover: anime.node.main_picture.large !== undefined ? anime.node.main_picture.large : anime.node.main_picture.medium,
+                    titles: { original: anime.node.title , ...anime.node.alternative_titles },
+                    status: anime.node.status,
+                    type: anime.node.media_type,
+                    wasWatched: false,
+                };
+            }
 
-        // Check if the anime was watched
-        if(anime.list_status.status === 'completed' || anime.list_status.status === 'watching') {
-            additionalData[anime.node.id].wasWatched = true;
+            // Check if the anime was watched
+            if(anime.list_status.status === 'completed' || anime.list_status.status === 'watching') {
+                additionalData[anime.node.id].wasWatched = true;
 
-            // Add the score
-            switch(user) {
-                case tiralex:
-                    additionalData[anime.node.id].scores.A = anime.list_status.score;
-                    break;
+                // Add the score
+                switch(user) {
+                    case tiralex:
+                        additionalData[anime.node.id].scores.A = anime.list_status.score;
+                        break;
 
-                case cycy:
-                    additionalData[anime.node.id].scores.C = anime.list_status.score;
-                    break;
+                    case cycy:
+                        additionalData[anime.node.id].scores.C = anime.list_status.score;
+                        break;
 
-                case leo:
-                    additionalData[anime.node.id].scores.L = anime.list_status.score;
-                    break;
+                    case leo:
+                        additionalData[anime.node.id].scores.L = anime.list_status.score;
+                        break;
 
-                case gyrehio:
-                    additionalData[anime.node.id].scores.V = anime.list_status.score;
-                    break;
+                    case gyrehio:
+                        additionalData[anime.node.id].scores.V = anime.list_status.score;
+                        break;
 
-                case tchm:
-                    additionalData[anime.node.id].scores.T = anime.list_status.score;
-                    break;
+                    case tchm:
+                        additionalData[anime.node.id].scores.T = anime.list_status.score;
+                        break;
+                }
             }
         }
     }
+
+    await fsPromise.writeFile('additional-data.json', JSON.stringify(additionalData));
+
+    // Compress the file
+    await Promise.all([
+        gzipFile('additional-data.json', 'additional-data.json.gz'),
+        brotliFile('additional-data.json', 'additional-data.json.br'),
+    ]);
+
+    console.log('Generated API v1.');
 }
 
-writeFileSync('additional-data.json', JSON.stringify(additionalData));
+/**
+ * Generates the API v2 data
+ */
+async function generateApiV2() {
+    // Fetch data from Tiralex1's API
+    const tiralexJson = await fetch('https://raw.githubusercontent.com/Tiralex1/ACLV/main/data.json').then(res => res.json());
 
-// Compress the file
+    // Create directories
+    mkdirSync('api/v2/animes', { recursive: true });
+
+    // Create a map of anime id to animes
+    const mergedData = [];
+
+    // Add the data for each user
+    for(const user of [tiralex, cycy, leo, gyrehio, tchm]) {
+        for(const anime of user) {
+            // Get the current state of the anime
+            let currentState = mergedData.find(a => a.id === anime.node.id);
+
+            if(currentState === undefined) {
+                currentState = {
+                    id: anime.node.id,
+                    scores: {},
+                    cover: anime.node.main_picture.large !== undefined ? anime.node.main_picture.large : anime.node.main_picture.medium,
+                    titles: { original: anime.node.title , ...anime.node.alternative_titles },
+                    status: anime.node.status,
+                    type: anime.node.media_type,
+                    wasWatched: false,
+                    synopsis: anime.node.synopsis,
+                    startDate: anime.node.start_date,
+                    endDate: anime.node.end_date,
+                    startSeason: anime.node.start_season,
+                    studios: anime.node.studios,
+                    episodes: anime.node.num_episodes,
+                    music: tiralexJson.anime.find(a => a.mal_id === anime.node.id)?.musique.map(m => { return { type: m.type, name: m.nom, artist: m.artiste, link: m.lien, number: m.numero } }),
+                };
+
+                mergedData.push(currentState);
+            }
+
+            // Check if the anime was watched
+            if(anime.list_status.status === 'completed' || anime.list_status.status === 'watching') {
+                currentState.wasWatched = true;
+
+                // Add the score
+                switch(user) {
+                    case tiralex:
+                        currentState.scores.A = anime.list_status.score;
+                        break;
+
+                    case cycy:
+                        currentState.scores.C = anime.list_status.score;
+                        break;
+
+                    case leo:
+                        currentState.scores.L = anime.list_status.score;
+                        break;
+
+                    case gyrehio:
+                        currentState.scores.V = anime.list_status.score;
+                        break;
+
+                    case tchm:
+                        currentState.scores.T = anime.list_status.score;
+                        break;
+                }
+            }
+        }
+    }
+
+    // Generate a file for each anime and an index file
+    const index = [];
+    const promises = [];
+
+    for(const anime of mergedData) {
+        // Add the anime to the index
+        index.push({
+            id: anime.id,
+            scores: anime.scores,
+            cover: anime.cover,
+            titles: anime.titles,
+            status: anime.status,
+            type: anime.type,
+            wasWatched: anime.wasWatched,
+            music: anime.music === undefined ? [] : anime.music,
+        });
+
+        // Generate the secondary file
+        await fsPromise.writeFile(`api/v2/animes/${anime.id}.json`, JSON.stringify({
+            synopsis: anime.synopsis,
+            startDate: anime.startDate,
+            endDate: anime.endDate,
+            startSeason: anime.startSeason,
+            studios: anime.studios,
+        }));
+
+        // Compress the file
+        promises.push(gzipFile(`api/v2/animes/${anime.id}.json`, `api/v2/animes/${anime.id}.json.gz`));
+        promises.push(brotliFile(`api/v2/animes/${anime.id}.json`, `api/v2/animes/${anime.id}.json.br`));
+    }
+
+    // Generate the index file
+    await fsPromise.writeFile('api/v2/index.json', JSON.stringify(index));
+    promises.push(gzipFile('api/v2/index.json', 'api/v2/index.json.gz'));
+    promises.push(brotliFile('api/v2/index.json', 'api/v2/index.json.br'));
+
+    await Promise.all(promises);
+
+    console.log('Generated API v2.');
+}
+
 await Promise.all([
-    gzipFile('additional-data.json', 'additional-data.json.gz'),
-    brotliFile('additional-data.json', 'additional-data.json.br'),
+    generateApiV1(),
+    generateApiV2(),
 ]);
 
 console.log('Done.');
