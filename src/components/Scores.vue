905487b04b7ed2017492847cc32236dbeb8ed0c2
ttcchhmm <60962195+ttcchhmm@users.noreply.github.com>
2024-05-09 19:08:49 +0200

    Display a help message when clicking on a score

--- src/components/Scores.vue
+++ src/components/Scores.vue
@@ -38,18 +38,63 @@ function getStatusClass(entry: UserEntry | undefined) {
     }
 }
 
+function displayMoreDetails(name: string, entry: UserEntry | undefined) {
+    let msg = `${name}\n`;
+
+    if(entry != undefined) {
+        // Score
+        if(entry.rating == undefined || entry.rating == 0) {
+            msg += '\n- Not scored';
+        } else {
+            msg += `\n- Scored ${entry.rating}/10`;
+        }
+
+        // Status
+        switch(entry.status) {
+            case 'watching': {
+                msg += '\n- Currently watching';
+                break;
+            }
+
+            case 'completed': {
+                msg += '\n- Finished';
+                break;
+            }
+
+            case 'on_hold': {
+                msg += '\n- On hold';
+                break;
+            }
+
+            case 'dropped': {
+                msg += '\n- Dropped';
+                break;
+            }
+
+            case 'plan_to_watch': {
+                msg += '\n- Plan to watch';
+                break;
+            }
+        }
+    } else {
+        msg += '\n- Not watched';
+    }
+
+    alert(msg);
+}
+
 </script>
 
 <template>
     <table class="watchedTable" v-once>
         <tbody>
             <tr>
-                <td class="cursorHelp" :class="getStatusClass(props.scores.A)" title="Alexis">A <span v-if="props.scores.A !== undefined && props.scores.A.rating !== undefined">{{ props.scores.A.rating }}</span></td>
-                <td class="cursorHelp" :class="getStatusClass(props.scores.C)" title="Cyprien">C <span v-if="props.scores.C !== undefined && props.scores.C.rating !== undefined">{{ props.scores.C.rating }}</span></td>
-                <td class="cursorHelp" :class="getStatusClass(props.scores.L)" title="Léonard">L <span v-if="props.scores.L !== undefined && props.scores.L.rating !== undefined">{{ props.scores.L.rating }}</span></td>
-                <td class="cursorHelp" :class="getStatusClass(props.scores.V)" title="Victor">V <span v-if="props.scores.V !== undefined && props.scores.V.rating !== undefined">{{ props.scores.V.rating }}</span></td>
-                <td class="cursorHelp" :class="getStatusClass(props.scores.T)" title="Tom">T <span v-if="props.scores.T !== undefined && props.scores.T.rating !== undefined">{{ props.scores.T.rating }}</span></td>
-                <td class="cursorHelp" :class="getStatusClass(props.scores.Q)" title="Quentin">Q <span v-if="props.scores.Q !== undefined && props.scores.Q.rating !== undefined">{{ props.scores.Q.rating }}</span></td>
+                <td class="cursorHelp" :class="getStatusClass(props.scores.A)" @click="displayMoreDetails('Alexis', props.scores.A)" title="Alexis">A <span v-if="props.scores.A !== undefined && props.scores.A.rating !== undefined">{{ props.scores.A.rating }}</span></td>
+                <td class="cursorHelp" :class="getStatusClass(props.scores.C)" @click="displayMoreDetails('Cyprien', props.scores.C)" title="Cyprien">C <span v-if="props.scores.C !== undefined && props.scores.C.rating !== undefined">{{ props.scores.C.rating }}</span></td>
+                <td class="cursorHelp" :class="getStatusClass(props.scores.L)" @click="displayMoreDetails('Léonard', props.scores.L)" title="Léonard">L <span v-if="props.scores.L !== undefined && props.scores.L.rating !== undefined">{{ props.scores.L.rating }}</span></td>
+                <td class="cursorHelp" :class="getStatusClass(props.scores.V)" @click="displayMoreDetails('Victor', props.scores.V)" title="Victor">V <span v-if="props.scores.V !== undefined && props.scores.V.rating !== undefined">{{ props.scores.V.rating }}</span></td>
+                <td class="cursorHelp" :class="getStatusClass(props.scores.T)" @click="displayMoreDetails('Tom', props.scores.T)" title="Tom">T <span v-if="props.scores.T !== undefined && props.scores.T.rating !== undefined">{{ props.scores.T.rating }}</span></td>
+                <td class="cursorHelp" :class="getStatusClass(props.scores.Q)" @click="displayMoreDetails('Quentin', props.scores.Q)" title="Quentin">Q <span v-if="props.scores.Q !== undefined && props.scores.Q.rating !== undefined">{{ props.scores.Q.rating }}</span></td>
             </tr>
         </tbody>
     </table>
